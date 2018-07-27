3437283d0a4c790b30f2412b99c9a1a03fef6b1f
enriquebox <ebox@socites.com>
2018-07-27 12:16:31 -0300

    Initial setup

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 require('colors');
 
-const source = '/Users/enriquebox/Documents/code/efan/build/superliga.phonegap/production/ios/spa';
-const destination = '/Users/enriquebox/Documents/code/apps/superliga.ios';
+const application = 'superliga';
+const os = 'android';
+const environment = 'production';
+const language = 'spa';
+const platform = 'phonegap';
+
+let source = '/Users/enriquebox/Documents/code/efan/build';
+let destination = '/Users/enriquebox/Documents/code/apps';
+
+source = `${source}/${application}.${platform}/${environment}/${os}/${language}`;
+destination = `${destination}/${application}.${os}`;
+
 const git = require('simple-git/promise')(destination);
 
 const {promisify} = require('util');
 const ncp = promisify(require('ncp').ncp);
 
-const version = '1.0';
+const version = '1.0.4';
+
+console.log(`Deploying ${application} ${platform} - ${environment}`.bold);
+console.log(`\tFrom: ${source}`.green);
+console.log(`\tTo: ${destination}\n`.green);
 
 (async function () {
 
     try {
         console.log('Pulling to avoid merge conflicts');
         await git.pull('origin', 'master');
-        console.log('\tOk');
+        console.log('\tOk'.green);
     } catch (exc) {
-        console.log('Error pulling branch', exc.stack.message);
+        console.log('Error pulling branch'.red, exc.stack.message);
     }
 
     try {
         console.log('Remove all previous files of the repository');
         await git.rm('./*');
-        console.log('\tPrevious files are removed');
+        console.log('\tPrevious files are removed'.green);
     } catch (exc) {
-        console.log('Error removing git files', exc.message);
+        console.log('Error removing git files'.red, exc.message);
     }
 
     try {
         console.log('Copy all application files');
         await ncp(source, destination);
-        console.log('\tAll application files are copied');
+        console.log('\tAll application files are copied'.green);
     } catch (exc) {
-        console.log('Error copying application files', exc.stack);
+        console.log('Error copying application files'.red, exc.stack);
         return;
     }
 
     try {
         console.log('Add all files to the repository');
         await git.add('*');
-        console.log('\tFiles added to repository');
+        console.log('\tFiles added to repository'.green);
     } catch (exc) {
-        console.log('Error adding git files', exc.message);
+        console.log('Error adding git files'.red, exc.message);
         return;
     }
 
@@ -50,9 +64,9 @@ const version = '1.0';
         try {
             console.log('Commit changes');
             await git.commit(`Version ${version}`);
-            console.log('\tChanges commited! :)');
+            console.log('\tChanges commited! :)'.green);
         } catch (exc) {
-            console.log('Error pushing changes', exc.message);
+            console.log('Error pushing changes'.red, exc.message);
         }
 
     }
@@ -61,22 +75,23 @@ const version = '1.0';
         console.log('Check for changes');
         let status = await git.status();
         if (!status.modified) {
-            console.log('Repository has no changes');
+            console.log('\tRepository has no changes'.green);
         } else {
-            console.log(`${status.not_added.length} files added to the repository`);
+            console.log(`\t${status.not_added.length} files added to the repository`.green);
             await commit();
         }
     } catch (exc) {
-        console.log('Error checking repository status', exc.message);
+        console.log('Error checking repository status'.red, exc.message);
         return;
     }
 
     try {
         console.log('Push changes');
         await git.push('origin', 'master');
-        console.log('\nAll steps done! :)'.green);
+        console.log('\tOk'.green);
+        console.log('\nAll steps done! :)'.bold);
     } catch (exc) {
-        console.log('Error pushing changes', exc.message);
+        console.log('Error pushing changes'.red, exc.message);
     }
 
 })();
